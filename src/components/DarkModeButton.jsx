1bdb47850449738890c8a38555817f0128ab30fb
hirodinn <hirebikila1@gmail.com>
2025-10-21 17:16:46 +0300

    do the icon and fix interactivity

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -7,7 +7,14 @@ export function DarkModeButton({ isDark, setIsDark }) {
         setIsDark(!isDark);
       }}
     >
-      DarkMode
+      {isDark ? (
+        <i
+          class="fa-solid fa-sun"
+          style={{ color: "rgba(231, 202, 148, 1)" }}
+        ></i>
+      ) : (
+        <i class="fa-solid fa-moon"></i>
+      )}
     </button>
   );
 }
